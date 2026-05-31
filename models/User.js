0591bc6cf9fc21872155a9127c51406a073d651f
Moataz <mtzwsam1aa@gmail.com>
2026-05-31 16:09:13 +0300

    fix user model quotes

--- models/User.js
+++ models/User.js
@@ -1,4 +1,4 @@
-const mongoose = require(“mongoose”);
+const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
 username: {
     type: String,
@@ -17,4 +17,4 @@ password: {
     required: true
 }
 }, { timestamps: true });
-module.exports = mongoose.model(“User”, userSchema);
+module.exports = mongoose.model("User", userSchema);
