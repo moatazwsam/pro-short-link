1cc57e7b841922106da9685d657d3e8032b7f93e
Moataz <mtzwsam1aa@gmail.com>
2026-06-02 20:08:05 +0300

    Added CPM ad page

--- index.js
+++ index.js
@@ -359,7 +359,7 @@ app.get("/stats", auth, async (req, res) => {
 
 });
 const PORT = process.env.PORT || 3000;
-app.get("/dashboard", auth, (req, res) => {
+app.get("/dashboard",(req, res) => {
 
     res.sendFile(
         path.join(__dirname, "public", "dashboard.html")
